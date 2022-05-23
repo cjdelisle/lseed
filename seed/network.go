a8a14e268a5dc26a175353b1aa94907bb1c45971
Caleb James DeLisle <cjd@cjdns.fr>
2022-05-23 22:24:00 +0200

    Allow cjdns addresses

--- seed/network.go
+++ seed/network.go
@@ -344,7 +344,7 @@ func init() {
 		"169.254.0.0/16", // RFC3927 link-local
 		"::1/128",        // IPv6 loopback
 		"fe80::/10",      // IPv6 link-local
-		"fc00::/7",       // IPv6 unique local addr
+		//"fc00::/7",       // IPv6 unique local addr
 	} {
 		_, block, err := net.ParseCIDR(cidr)
 		if err != nil {
